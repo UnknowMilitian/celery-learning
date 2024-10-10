b89eb12ea18e0fa0fb45e4b4e6c842f6edfb4495
UnknowMilitiant <j.umariy4238@gmail.com>
2024-10-10 17:40:20 +0500

    all that shit is work, only i have should to do was write to cmd was:  celery -A core worker -l info -P solo

--- core/core/settings.py
+++ core/core/settings.py
@@ -133,6 +133,7 @@ CELERY_RESULT_BACKEND = "redis://" + REDIS_HOST + ":" + REDIS_PORT + "/0"
 CELERY_ACCEPT_CONTENT = ["application/json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
+CELERY_TASK_ALWAYS_EAGER = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- core/main/tasks.py
+++ core/main/tasks.py
@@ -1,23 +1,39 @@
+# from django.core.mail import send_mail
+# from core.celery import app
+
+# from .service import send
+# from .models import Contact
+
+
+# @app.task
+# def test_task():
+#     print("Test task executed successfully")
+
+
+# @app.task(bind=True)
+# def send_spam_email(self, user_email):
+#     try:
+#         if user_email:
+#             send(user_email)
+#             self.logger.info(f"Email sent to {user_email}")
+#         else:
+#             self.logger.warning("No email provided")
+#     except Exception as e:
+#         self.logger.error(f"Failed to send email: {str(e)}")
+#         raise e
+
+
+# tasks.py
+from celery import shared_task
 from django.core.mail import send_mail
-from core.celery import app
-
-from .service import send
-from .models import Contact
 
 
-@app.task
-def test_task():
-    print("Test task executed successfully")
-
-
-@app.task(bind=True)
-def send_spam_email(self, user_email):
-    try:
-        if user_email:
-            send(user_email)
-            self.logger.info(f"Email sent to {user_email}")
-        else:
-            self.logger.warning("No email provided")
-    except Exception as e:
-        self.logger.error(f"Failed to send email: {str(e)}")
-        raise e
+@shared_task
+def send_spam_email(user_email):
+    send_mail(
+        "Welcome!",
+        "Thank you for registering!",
+        "from@example.com",
+        [user_email],
+        fail_silently=False,
+    )
