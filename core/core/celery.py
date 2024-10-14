f936d27a2bee51a5e9db2b0b0d008f55de71b541
UnknowMilitiant <j.umariy4238@gmail.com>
2024-10-14 12:10:20 +0500

    Completed

--- core/core/celery.py
+++ core/core/celery.py
@@ -7,3 +7,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 app = Celery("core")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "send-spam-every-1-minute": {
+        "task": "main.tasks.send_beat_email",
+        "schedule": crontab(minute="*/1"),
+    }
+}
--- core/main/tasks.py
+++ core/main/tasks.py
@@ -4,10 +4,19 @@ from core.celery import app
 from .service import send
 from .models import Contact
 
+
 @app.task
 def send_spam_email(user_email):
-  send(user_email)
+    send(user_email)
 
 
-# @app.task
-# def send_
+@app.task
+def send_beat_email():
+    for contact in Contact.objects.all():
+        send_mail(
+            "You've subscribed to something",
+            "We'll send message each 1-minutes",
+            "j.umariy4238@gmail.com",
+            [contact.email],
+            fail_silently=False,
+        )
